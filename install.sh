0c93c3562606939577e6818482125c862105364d
Shadow Walker <66953516+shadowwalker005@users.noreply.github.com>
2020-06-22 19:58:23 +0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#colors
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+cyan=$(tput setaf 6)
+bold=$(tput bold 5)
+clear
+
+echo
+echo "${yellow}${bold} ~ Installating PayGen... "
+echo
+sleep 2
+chmod 777 paygen
+echo
+cp paygen $PREFIX/bin
+echo
+sleep 1
+echo "${green}${bold} ~ PayGen Successfully Installed"
+echo
+sleep 1
+echo "${cyan}${bold} ~ Type paygen Anywhere To Run PayGen "
+echo 
+sleep 1
+echo "${cyan}${bold} ~ Made By Shadow Walker "
+echo 
